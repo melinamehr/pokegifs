4dc39732b570d0e3aaf10f57663e88bb47d35765
MELINA MEHR <MELINAMEHR@MSN.COM>
2017-06-28 16:00:50 -0400

    printing json name, type name, and id

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -1,8 +1,21 @@
 class PokemonController < ApplicationController
 
-  def index
-    render json: { "message": "ok" }
+  def show
+  res = Typhoeus.get("http://pokeapi.co/api/v2/pokemon/"+params[:id], followlocation: true)
+  body = JSON.parse(res.body)
+
+  render json: {
+    results:[
+    body["name"],body["types"][0]['type']['name'], body["id"]
+    ]}
 
   end
-
 end
+
+
+
+  # body = JSON.parse(res.body)
+  # body["pokemon"]
+  #   puts poke["id"]
+  #   puts poke["name"]
+  #   puts poke ["type"]
