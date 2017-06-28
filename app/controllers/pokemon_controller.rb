678ea51163e1e8a936f833fa68f1a79da050f12f
MELINA MEHR <MELINAMEHR@MSN.COM>
2017-06-28 17:47:08 -0400

    gif url loading properly

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -7,19 +7,21 @@ class PokemonController < ApplicationController
   #body["name"] # 'bulbasaur' name gotten from ^^^ first api call
 
   #second_response = Typhoeus.get("http://giphfy....?name=#{body['name']}")
-  "name":body["name"]
 
-  res_gif = Typhoeus.get("https://api.giphy.com/v1/gifs/search?api_key=api_key=#{ENV["GIPHY_KEY"]}&q="+params[:id], followlocation: true)
+
+  res_gif = Typhoeus.get("https://api.giphy.com/v1/gifs/search?api_key=#{ENV['GIPHY_KEY']}&q=#{body["name"]}", followlocation: true)
   body_gif = JSON.parse(res_gif.body)
 
   render json:
    {
+    #  "url":body_gif["embed_url"].map do |type_gif|
+    #    type_gif["body_gif"]["url"]
     url: body_gif["data"][0]["embed_url"],
-    "name":body["name"],
-    "id":body["id"],
-    "type":body["types"].map do |type_hash|
+    name: body["name"],
+    id: body["id"],
+    type: body["types"].map do |type_hash|
       type_hash["type"]["name"]
-        end
+    end
     }
   end
 end
