c74af7c3f253afb865d4c629822d6d109efa9e33
MELINA MEHR <MELINAMEHR@MSN.COM>
2017-06-28 17:25:23 -0400

    added key to bash

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -4,12 +4,21 @@ class PokemonController < ApplicationController
   res = Typhoeus.get("http://pokeapi.co/api/v2/pokemon/"+params[:id], followlocation: true)
   body = JSON.parse(res.body)
 
-  render json: {
+  #body["name"] # 'bulbasaur' name gotten from ^^^ first api call
+
+  #second_response = Typhoeus.get("http://giphfy....?name=#{body['name']}")
+  "name":body["name"]
+
+  res_gif = Typhoeus.get("https://api.giphy.com/v1/gifs/search?api_key=api_key=#{ENV["GIPHY_KEY"]}&q="+params[:id], followlocation: true)
+  body_gif = JSON.parse(res_gif.body)
+
+  render json:
+   {
+    url: body_gif["data"][0]["embed_url"],
     "name":body["name"],
     "id":body["id"],
     "type":body["types"].map do |type_hash|
       type_hash["type"]["name"]
-
         end
     }
   end
